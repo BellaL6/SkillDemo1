66bd6d3fc1d43e7181db7703db49993aeab9e4cd
BellaL6 <jyli@ucsd.edu>
2022-05-01 18:56:34 -0700

    Update skillDemo1Tester.java

--- skillDemo1Tester.java
+++ skillDemo1Tester.java
@@ -6,7 +6,7 @@ public class skillDemo1Tester {
     @Test
     public void multiplication() {
 
-        assertEquals("Test for the failure case: ", 10, SkillDemo1.multiplication(2,6));
+        assertEquals("Test for the success case: ", 10, SkillDemo1.multiplication(2,5));
     }
     
 }
